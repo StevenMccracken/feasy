573b25b0bbcf23df4c4bc94099fa8a9c42e62a7f
Daniel Olivera <dannyolivera11@gmail.com>
2016-11-21 16:02:35 -0800

    testAPIs

---
+++ testAPI/RestAPi/RestAPi/app/models/bear.js
@@ -0,0 +1,9 @@
+﻿var mongoose = require('mongoose');
+//var Schema = mongoose.Schema;
+
+var BearSchema = mongoose.Schema({
+    //bears name
+    name: String
+});
+
+module.exports = mongoose.model('Bear',BearSchema);
