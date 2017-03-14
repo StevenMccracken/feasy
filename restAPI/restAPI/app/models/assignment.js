c093ac6ad53d1de4a2533fefd91a7f2638022874
Steven McCracken <stevenamccracken@gmail.com>
2017-03-13 17:45:34 -0700

    Added backend server code
    Copied Danny Olivera's backend server code. Lost git commit history but the code is here and the REST APIs work correctly. Also updated the gitignore for angular 2 development

---
+++ restAPI/restAPI/app/models/assignment.js
@@ -0,0 +1,12 @@
+﻿var mongoose = require('mongoose');
+
+var AssignmentSchema = mongoose.Schema({
+    type: { type: String, required: true },
+    name: { type: String, required: true },
+    dueDate: { type: Date },
+    assignmentId: { type: Number, required: true, unique: true } //assignemts id 
+    //userid
+    //finish this template, check with steven for other values
+});
+
+module.exports = mongoose.model('Assignment', AssignmentSchema);
