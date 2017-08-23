6c11c324e262a1f22c7324c36afd143254cfcb4b
Daniel Olivera <dannyolivera11@gmail.com>
2017-08-22 23:23:06 -0700

    added google calendar callback uris and scopes

--- backend/config/google.js
+++ backend/config/google.js
@@ -7,6 +7,8 @@ const CONFIG = require('./googleSecret');
 module.exports = {
   secret: CONFIG.web['client_secret'],
   clientId: CONFIG.web['client_id'],
-  redirectUri: CONFIG.web['redirect_uris'][0],
-  scope: 'openid profile email',
+  redirectUri: CONFIG.web['redirect_uris'][2],
+  scope:'openid email profile',
+  calendarRedirectUri: CONFIG.web['redirect_uris'][1],
+  calendarScope: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/plus.login'
 };
