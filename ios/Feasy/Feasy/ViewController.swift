f7f20d9c5c7470c5e6c6fe1b7cac834e265fb9a5
Steven McCracken <stevenamccracken@gmail.com>
2017-09-14 18:28:54 -0700

    Initial project setup

---
+++ ios/Feasy/Feasy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feasy
+//
+//  Created by Steven McCracken on 9/14/17.
+//  Copyright © 2017 Feasy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
