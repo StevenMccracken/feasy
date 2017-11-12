d05350febfacf3dd7e10567148ab9b856e57b190
Steven McCracken <stevenamccracken@gmail.com>
2017-11-12 10:15:54 -0800

    Configured database authentication

--- backend/config/database.js
+++ backend/config/database.js
@@ -12,13 +12,8 @@ const username = CONFIG.username;
 const password = CONFIG.password;
 const uri = 'mongodb://localhost/userDB';
 const connectionOptions = {
-  user: username,
-  pass: password,
   useMongoClient: true,
   promiseLibrary: BLUEBIRD,
-  auth: {
-    authSorce: 'userDB',
-  },
 };
 
 /**
@@ -32,12 +27,20 @@ const isConnected = function isConnected() {
 /**
  * connect - Attempts to open a Mongoose database connection. If
  * a connection already exists, this function resolves immediately
+ * @param {boolean} [_isProductionEnvironment = false] determines
+ * whether or not to connect to the database with authentication or not
  * @return {Promise} an empty promise when the connection is established
  */
-const connect = function connect() {
+const connect = function connect(_isProductionEnvironment = false) {
   const promise = new Promise((resolve, reject) => {
     if (isConnected()) resolve();
     else {
+      if (_isProductionEnvironment) {
+        connectionOptions.user = username;
+        connectionOptions.pass = password;
+        connectionOptions.auth = { authSorce: 'userDB' };
+      }
+
       MONGOOSE.connect(uri, connectionOptions)
         .then(
           () => {
